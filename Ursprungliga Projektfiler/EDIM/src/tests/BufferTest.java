63fe5cdb793ca3c73f1f49030e72296ae94ceeb9
benny1051 <81147660+benny1051@users.noreply.github.com>
2022-02-15 14:55:22 +0100

    Userregistertest

--- Ursprungliga Projektfiler/EDIM/src/tests/BufferTest.java
+++ Ursprungliga Projektfiler/EDIM/src/tests/BufferTest.java
@@ -1,5 +1,5 @@
 package tests;
-
+//Benny
 import server.Buffer;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
--- Ursprungliga Projektfiler/EDIM/src/tests/UserTest.java
+++ Ursprungliga Projektfiler/EDIM/src/tests/UserTest.java
@@ -1,5 +1,5 @@
 package tests;
-
+//Benny
 
 import org.junit.jupiter.api.Test;
 import server.Activity;
