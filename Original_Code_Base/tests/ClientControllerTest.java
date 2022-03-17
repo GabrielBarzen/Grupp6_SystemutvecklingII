8f2f065385c39db72af6061e604a6ddebe413cdc
Ardian Glamniki <ardian.glamniki@hotmail.com>
2022-03-17 14:38:36 +0100

    update ClientControllerTest

--- Original_Code_Base/tests/ClientControllerTest.java
+++ Original_Code_Base/tests/ClientControllerTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @author Ardian Glamniki
+ */
 class ClientControllerTest {
     private ClientController clientController;
 
